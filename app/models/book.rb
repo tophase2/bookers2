1dd9388c6232271f529322401db1eb7d0972b75f
EC2 Default User <ec2-user@ip-172-31-3-38.ap-northeast-1.compute.internal>
2023-04-14 00:43:07 +0000

    [add]投稿一覧追加

---
+++ app/models/book.rb
@@ -0,0 +1,14 @@
+class Book < ApplicationRecord
+  
+  has_one_attached :profile_image
+  belongs_to :user
+  
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  resources :books
   get "/homes/about" => "homes#about", as: "about"
 end
