1d362f874be7c52d8a05d2193a2fc5ab36862ce4
EC2 Default User <ec2-user@ip-172-31-3-38.ap-northeast-1.compute.internal>
2023-04-18 21:21:40 +0000

    [add]ユーザページ編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
-    @user = current_user.id
+    @user = User.find(current_user.id)
   end
 
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
+  
   def index
     @users = User.all
     @book = Book.new
     @books = Book.all
-    @user = current_user.id
+    @user = User.find(current_user.id)
   end
 
   def show
@@ -17,7 +18,7 @@ class UsersController < ApplicationController
   
   def update
     @user = User.find(params[:id])
-    @user.update
+    @user.update(user_params)
     redirect_to user_path(@user.id)
   end
 
