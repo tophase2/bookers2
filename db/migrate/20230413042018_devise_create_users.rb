e44352c1b0abd67016ecf0bfcbf53625205b73b0
EC2 Default User <ec2-user@ip-172-31-3-38.ap-northeast-1.compute.internal>
2023-04-29 07:43:19 +0000

    [add]マイグレーションファイル編集

--- db/migrate/20230413042018_devise_create_users.rb
+++ db/migrate/20230413042018_devise_create_users.rb
@@ -34,6 +34,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       
       t.string :name
       t.string :introduction
+      t.string :profile_image_id
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_16_125635) do
+ActiveRecord::Schema.define(version: 2023_04_13_092556) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -55,9 +55,10 @@ ActiveRecord::Schema.define(version: 2023_04_16_125635) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
+    t.string "introduction"
+    t.string "profile_image_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
